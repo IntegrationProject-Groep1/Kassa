8d07a33579aeb9369c2c61fc8d1708cb1fb6849d
GJeremy64 <luyckfasseel.jeremy@gmail.com>
2026-05-06 11:48:13 +0200

    style: fix persistent flake8 E303 in odoo_setup.py

--- integratie/odoo_setup.py
+++ integratie/odoo_setup.py
@@ -212,7 +212,6 @@ def ensure_kassa_addons(odoo_url: str, odoo_db: str, odoo_user: str, odoo_pass: 
         return False
 
     for addon_name in ["kassa_pos_custom"]:
-
         module_ids = _rpc(
             models,
             odoo_db,
